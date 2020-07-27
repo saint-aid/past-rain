d8ecc5887b3feeaefe64d7338c01ede72ada143b
rinomari <ysp622@gmail.com>
2020-07-27 19:12:04 +0900

    배경 dim 처리

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,32 +6,7 @@ const main = {
             this.search();
         });
         document.querySelector('#btn-excel').addEventListener('click',()=>{
-            if(!confirm("엑셀다운로드를 시작합니다.\n계속하시겠습니까?")) return;
-
-            const paraMap = {
-                city :  document.querySelector('#city').value,
-                searchStDay : document.querySelector('#searchStDay').value ,
-                searchEdDay : document.querySelector('#searchEdDay').value ,
-            };
-            console.log("엑셀 다운로드 !!!!! 시작 ", paraMap);
-
-            const xhr = new XMLHttpRequest();
-            xhr.open("POST", "/excelDown", true);
-            xhr.responseType = 'blob';
-            xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-            xhr.send(JSON.stringify(paraMap));
-            xhr.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    console.log("다운로드!!");
-                    var _data = this.response;
-                    var _blob = new Blob([_data], {type: 'ms-vnd/excel'});
-
-                    var link = document.createElement('a');
-                    link.href = window.URL.createObjectURL(_blob);
-                    link.download = 'text.xls';
-                    link.click();
-                }
-            }
+            this.excelDownload();
         });
         // $('#datetimepicker2').datetimepicker({
         //     locale: 'ru'
@@ -87,11 +62,37 @@ const main = {
             console.log('Caught Exception: ' + e.description);
         }
     },
+    excelDownload : () => {
+        if(!confirm("엑셀다운로드를 시작합니다.\n계속하시겠습니까?")) return;
+        $(".m_bg").show(); //배경 //jQuery 걷어내기
+        const paraMap = {
+            city :  document.querySelector('#city').value,
+            searchStDay : document.querySelector('#searchStDay').value ,
+            searchEdDay : document.querySelector('#searchEdDay').value ,
+        };
+        console.log("엑셀 다운로드 !!!!! 시작 ", paraMap);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "/excelDown", true);
+        xhr.responseType = 'blob';
+        xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        xhr.send(JSON.stringify(paraMap));
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                $(".m_bg").hide(); // 배경 //jQuery 걷어내기
+                console.log("다운로드!!");
+                var _data = this.response;
+                var _blob = new Blob([_data], {type: 'ms-vnd/excel'});
+                //--가상다운--//
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(_blob);
+                link.download = 'text.xls';
+                link.click();
+            }
+        }
+    },
 };
 
-function expireCookie(name) {
-    document.cookie = name + "=FALSE; path=/; expires=" + new Date( 0 ).toUTCString();
-}
 
 main.init();
 
