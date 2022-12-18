f7d5dbd742fd318caa9e70ebe9b53a317e668dab
Feelyoon <ysp622@gmail.com>
2022-12-18 15:38:43 +0900

    자바스크립트 리팩토링

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,88 +1,87 @@
-`use strict`
 
-const main = {
-    init : function(){
-        document.querySelector('#btn-search').addEventListener('click',()=>{
-            this.search();
-        });
-        document.querySelector('#btn-excel').addEventListener('click',()=>{
-            this.excelDownload();
-        });
-    },
-    search : function () {
-        const param = {
-            city :  document.querySelector('#city').value,
-            searchStDay : document.querySelector('#searchStDay').value ,
-            searchEdDay : document.querySelector('#searchEdDay').value ,
-        };
-        $(".m_bg").show(); //배경 //jQuery 걷어내기
-        $(".loadingImg").show(); //배경 //jQuery 걷어내기
+const main = (() => {
+    `use strict`
+    document.querySelector('#btn-search').addEventListener('click', () => {
+        main.search();
+    });
+    document.querySelector('#btn-excel').addEventListener('click', () => {
+        main.excelDownload();
+    });
 
-        fetch('/getRainAll', {
-          method: 'POST',
-          body: JSON.stringify(param),
-          headers: {'Content-Type' :'application/json;charset=utf-8;'}
-        })
-        .then(response => response.json()) //json 객체로 반환
-        .then( result => {
-            $(".m_bg").hide(); //배경 //jQuery 걷어내기
-            $(".loadingImg").hide(); //배경 //jQuery 걷어내기
-            const list = result.data;
+    return {
+        search: function () {
+            const param = {
+                city: document.querySelector('#city').value,
+                searchStDay: document.querySelector('#searchStDay').value,
+                searchEdDay: document.querySelector('#searchEdDay').value,
+            };
+            // $(".m_bg").show(); //배경 //jQuery 걷어내기
+            // $(".loadingImg").show(); //배경 //jQuery 걷어내기
+            document.querySelector('.m_bg').display = 'block'
+            document.querySelector('.loadingImg').display = 'block'
 
-            let subTitle = list[0].subTitle;
-            console.log('subtitle ---', document.querySelector('#subTitle'));
-            document.querySelector('#subTitle').innerHTML = subTitle;
+            fetch('/getRainAll', {
+                method: 'POST',
+                body: JSON.stringify(param),
+                headers: {'Content-Type': 'application/json;charset=utf-8;'}
+            })
+                .then(response => response.json()) //json 객체로 반환
+                .then(result => {
+                    $(".m_bg").hide(); //배경 //jQuery 걷어내기
+                    $(".loadingImg").hide(); //배경 //jQuery 걷어내기
+                    const list = result.data;
 
-            document.querySelector('#tbody tr').remove();
-            let html ="";
-            for(let i in list){
-                html += "<tr>";
-                html +="<td>"+list[i].searchDay+"</td>";
-                html +="<td>"+list[i].rainHm+"</td>";
-                html +="<td>"+list[i].rainYn+"</td>";
-                html +="<td>"+list[i].rain15m+"</td>";
-                html +="<td>"+list[i].rain60m+"</td>";
-                html +="<td>"+list[i].rain3h+"</td>";
-                html +="<td>"+list[i].rain6h+"</td>";
-                html +="<td>"+list[i].rain12h+"</td>";
-                html +="<td>"+list[i].temperature+"</td>";
-                html +="</tr>";
-            }
-            document.querySelector('#tbody').innerHTML = html;
-        })
-        .catch(error => console.error(error));
-    },
-    excelDownload : () => {
-        if(!confirm("엑셀다운로드를 시작합니다.\n계속하시겠습니까?")) return;
-        $(".m_bg").show(); //배경 //jQuery 걷어내기
-        $(".loadingImg").show(); //배경 //jQuery 걷어내기
-        const paraMap = {
-            city :  document.querySelector('#city').value,
-            searchStDay : document.querySelector('#searchStDay').value ,
-            searchEdDay : document.querySelector('#searchEdDay').value ,
-        };
-        console.log("엑셀 다운로드 !!!!! 시작 ", paraMap);
-        fetch('/excelDown', {
-            method: 'POST',
-            body: JSON.stringify(paraMap),
-            headers: {'Content-Type' :'application/json;charset=utf-8;'}
-        })
-        .then(response => response.blob())
-        .then(_data => {
-            $(".m_bg").hide(); //배경 //jQuery 걷어내기
-            $(".loadingImg").hide(); //배경 //jQuery 걷어내기
-            //console.log(_data);
-            const _blob = new Blob([_data], {type: 'ms-vnd/excel'});
-            //--가상다운--//
-            const link = document.createElement('a');
-            link.href = window.URL.createObjectURL(_blob);
-            link.download = 'rainy_excel.xls';
-            link.click();
-        })
-        .catch(error => console.error(error));
-    },
-};
+                    let subTitle = list[0].subTitle;
+                    console.log('subtitle ---', document.querySelector('#subTitle'));
+                    document.querySelector('#subTitle').innerHTML = subTitle;
 
-
-main.init();
-
+                    document.querySelector('#tbody tr').remove();
+                    let html = "";
+                    list.forEach( item => {
+                        html += `<tr>
+                                     <td>${item.searchDay}</td>
+                                     <td>${item.rainHm}</td>
+                                     <td>${item.rainYn}</td>
+                                     <td>${item.rain15m}</td>
+                                     <td>${item.rain60m}</td>
+                                     <td>${item.rain3h}</td>
+                                     <td>${item.rain6h}</td>
+                                     <td>${item.rain12h}</td>
+                                     <td>${item.temperature}</td>
+                                 </tr>`
+                    })
+                    document.querySelector('#tbody').innerHTML = html;
+                })
+                .catch(error => console.error(error));
+        },
+        excelDownload: function () {
+            if (!confirm("엑셀다운로드를 시작합니다.\n계속하시겠습니까?")) return;
+            $(".m_bg").show(); //배경 //jQuery 걷어내기
+            $(".loadingImg").show(); //배경 //jQuery 걷어내기
+            const paraMap = {
+                city: document.querySelector('#city').value,
+                searchStDay: document.querySelector('#searchStDay').value,
+                searchEdDay: document.querySelector('#searchEdDay').value,
+            };
+            console.log("엑셀 다운로드 !!!!! 시작 ", paraMap);
+            fetch('/excelDown', {
+                method: 'POST',
+                body: JSON.stringify(paraMap),
+                headers: {'Content-Type': 'application/json;charset=utf-8;'}
+            })
+                .then(response => response.blob())
+                .then(_data => {
+                    $(".m_bg").hide(); //배경 //jQuery 걷어내기
+                    $(".loadingImg").hide(); //배경 //jQuery 걷어내기
+                    //console.log(_data);
+                    const _blob = new Blob([_data], {type: 'ms-vnd/excel'});
+                    //--가상다운--//
+                    const link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(_blob);
+                    link.download = 'rainy_excel.xls';
+                    link.click();
+                })
+                .catch(error => console.error(error));
+        }
+    }
+})()
